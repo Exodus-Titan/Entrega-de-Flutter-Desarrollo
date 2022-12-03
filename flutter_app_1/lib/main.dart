3ef8a7ebc09c6ee9a175addb604fd4e8fd277189
David Liendo <Davidals70@gmail.com>
2022-12-03 06:41:55 -0400

    Detalle en main
    Debido al merge

--- flutter_app_1/lib/main.dart
+++ flutter_app_1/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pantalla_1/pantallas/pantalla_cursos.dart';
+import 'package:flutter_pantalla_1/pantallas/prueba_navegacion.dart';
+import 'package:flutter_pantalla_1/pantallas/prueba_navegacion_usuario.dart';
 
 void main() {
   runApp(MyApp());
@@ -41,6 +43,74 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.w600,
               color: Color(0xFF2196F3),
             )),
+      ),
+    );
+  }
+}
+
+class CursoApp extends StatelessWidget {
+  const CursoApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Prueba',
+      home: RootPage(),
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          textTheme: const TextTheme(
+              headline1: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                color: Color(0XFF030047),
+              ),
+              headline2: TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF5F5FFF),
+              ))),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({Key? key}) : super(key: key);
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
+  List<Widget> Pages = const [
+    Navegacion(),
+    ProfilePage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Pages[currentPage],
+      appBar: AppBar(
+        title: const Text('Corsi'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('Floating action button');
+        },
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        selectedIndex: currentPage,
       ),
     );
   }
