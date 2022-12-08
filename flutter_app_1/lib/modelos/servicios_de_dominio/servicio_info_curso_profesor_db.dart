373052f604d933186b774f7908ab659c30e7f2b2
David Liendo <Davidals70@gmail.com>
2022-12-07 21:40:10 -0400

    Mas pruebas

--- flutter_app_1/lib/modelos/servicios_de_dominio/servicio_info_curso_profesor_db.dart
+++ flutter_app_1/lib/modelos/servicios_de_dominio/servicio_info_curso_profesor_db.dart
@@ -2,13 +2,29 @@
 // import '../patron_iterador/iterado_generico/iterable_lista.dart';
 // import '../../data/Adaptador/I_Repositoty_BD.dart';
 // import '../patron_iterador/iterado_generico/iterable_lista.dart';
-
+// import '../patron_iterador/iterado_generico/iterador_lista.dart';
+// import '../../data/ModeloTamporal/cursoTemp.dart';
 
 // class ServicioGuardarInfoCursoProfesorDB {
 //   Future<void> guardarTodosLosCursosConProfesores(
-//       IRepositorioMoor adaptadorCursoProfesor, IterableLista<InfoCursoConProfesor> datos) async {
+//       IRepositorioMoor adaptadorCursoProfesor,
+//       IterableLista<InfoCursoConProfesor> datos) async {
+//     IteradorLista<InfoCursoConProfesor> iteradorDatos = datos.crearIterador();
 //     await adaptadorCursoProfesor.init();
-
+//     while(iteradorDatos.hasMore()) {
+//       adaptadorCursoProfesor.guardarTodosLosCursos(traducirCursos(iteradorDatos));
+//       adaptadorCursoProfesor.guardarTodosLosUsuarios(List<UsuarioTemp> usuario);
+//     }
 //     adaptadorCursoProfesor.close();
+//   }
+
+//   List<CursoTemp> traducirCursos(IteradorLista<InfoCursoConProfesor> cursos) {
+//     List<CursoTemp> cursosTemp = [];
+//     for (int cont = 0; cont < cursos.cantidadElementos(); cont++) {
+//       cursosTemp.add(cursosTemp(
+          
+//         ),
+//       );
+//     }
 //   }
 // }
